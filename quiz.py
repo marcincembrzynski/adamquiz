3e0391da167b1ce7b9f18825e543168ab91e2b91
marcincembrzynski <marcin@inandi.com>
2022-01-15 12:54:34 +0000

    initial

---
+++ quiz.py
@@ -0,0 +1,80 @@
+import random
+
+class Question:
+    def __init__(self, question, choices, correct):
+        self.question = question
+        self.choices = choices
+        self.correct = correct
+        self.kind = "test"
+
+    def isCorrect(self, value):
+        return self.correct == value
+
+    def printQuestion(self):
+        print("enter " + self.kind + " for: " + self.question)
+        print("---------------")
+        for choice in self.choices:
+            print(choice)
+        print("---------------")
+
+class SynQuestion(Question):
+    def __init__(self, question, choices, correct):
+        self.question = question
+        self.choices = choices
+        self.correct = correct
+        self.kind = "synonym"
+
+class NotSynQuestion(Question):
+    def __init__(self, choices, correct):
+        self.choices = choices
+        self.correct = correct
+        self.kind = "synonym"
+
+    def printQuestion(self):
+        print("Which word is out of place in this group of words: ")
+        print("---------------")
+        for choice in self.choices:
+            print(choice)
+        print("---------------")
+
+class AntQuestion(Question):
+    def __init__(self, question, choices, correct):
+        self.question = question
+        self.choices = choices
+        self.correct = correct
+        self.kind = "antonym"
+
+
+
+class Quiz:
+    def __init__(self, questions):
+        self.questions = questions
+
+
+    def test(self):
+        correct = 0
+        print(" ")
+        random.shuffle(self.questions)
+        item = 0
+        for question in self.questions:
+            item = item + 1
+            print("\n\n")
+            print(str(item) + " / " + str(len(self.questions)))
+            question.printQuestion()
+            answer = raw_input("answer:")
+            print(answer)
+            if question.isCorrect(answer):
+                print("---------------")
+                for x in range(10):
+                    print("ok!"* x)
+                print("---------------")
+                correct = correct + 1
+            else:
+                print("---------------")
+                print("correct answer: ", question.correct)
+                print("---------------")
+
+            raw_input("press any key to continue....")
+
+        print("correct: ", correct)
+        print("not correct: ", len(self.questions) - correct)
